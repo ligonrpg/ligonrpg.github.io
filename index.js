dd525d5f6104657f9fe7a24756db3fa69ddcd94a
ligonrpg <ligonrpgclub@gmail.com>
2024-09-03 08:01:37 -0400

    push changes to add redirect for chromebooks

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function load(){
+    let hasTouchScreen = false;
+
+    if ("maxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.maxTouchPoints > 0;
+    } 
+
+    if (hasTouchScreen) {
+        window.location.href = window.location.href + "chromebook"
+    }
+}
+
+
+
+
 function clicked(id){
     window.location.href = window.location.href + id;
 }
