861b336d8f567a4667fa860d3153b15abddac6a0
ligonrpg <ligonrpgclub@gmail.com>
2024-09-03 11:08:50 -0400

    button changes again fuck me in the a-

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function load(){
     let hasTouchScreen = false;
     let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-    document.getElementById('topheader').innerHTML = "t1"
-
     if ("maxTouchPoints" in navigator) {
         hasTouchScreen = navigator.maxTouchPoints > 0;
     } 
@@ -11,8 +9,6 @@ function load(){
     if (hasTouchScreen && !isMobile) {
         let stylesheet = document.getElementById('stylesheet');
         stylesheet.setAttribute('href', 'chromebook.css')
-        document.getElementById('topheader').setAttribute('innerHTML', "hello");
-        document.getElementById('topheader').innerHTML = "t";
     }
 
     if (hasTouchScreen && isMobile){
